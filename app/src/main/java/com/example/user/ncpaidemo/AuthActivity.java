80bace0ad39a389d8dcf656d2ecce3e8d4e7322c
changdol <edueman@naver.com>
2020-03-09 18:07:09 +0900

    OCR 인증정보 설정 추가

--- app/src/main/java/com/example/user/ncpaidemo/AuthActivity.java
+++ app/src/main/java/com/example/user/ncpaidemo/AuthActivity.java
@@ -28,6 +28,9 @@ public class AuthActivity extends BaseActivity {
         String chatbotApiGwUrl = sharedPref1.getString("chatbot_api_gw_url", "");
         String chatbotSecretKey = sharedPref1.getString("chatbot_secret_key", "");
 
+        String ocrApiGwUrl = sharedPref1.getString("ocr_api_gw_url", "");
+        String ocrSecretKey = sharedPref1.getString("ocr_secret_key", "");
+
         EditText editTextApplicationClientId = (EditText) findViewById(R.id.text_input_application_client_secret2);
         editTextApplicationClientId.setText(applicationClientId);
 
@@ -40,6 +43,12 @@ public class AuthActivity extends BaseActivity {
         EditText editTextChatbotSecretKey = (EditText) findViewById(R.id.text_input_chatbot_secret_key);
         editTextChatbotSecretKey.setText(chatbotSecretKey);
 
+        EditText editTextOCRApiGwUrl = (EditText) findViewById(R.id.text_input_ocr_api_gw_url);
+        editTextOCRApiGwUrl.setText(ocrApiGwUrl);
+
+        EditText editTextOcrSecretKey = (EditText) findViewById(R.id.text_input_ocr_secret_key);
+        editTextOcrSecretKey.setText(ocrSecretKey);
+
         TextView textView1 = (TextView) findViewById(R.id.textView_rlt) ;
         textView1.setText("");
 
@@ -51,7 +60,6 @@ public class AuthActivity extends BaseActivity {
             public void onClick(View v) {
 
                 try {
-
                     // 인증 정보 Save
                     EditText editTextClientId = (EditText) findViewById(R.id.text_input_application_client_secret2);
                     String clientIdInputText = editTextClientId.getText().toString();
@@ -65,6 +73,12 @@ public class AuthActivity extends BaseActivity {
                     EditText editTextSecretKey = (EditText) findViewById(R.id.text_input_chatbot_secret_key);
                     String clientSecretKey = editTextSecretKey.getText().toString();
 
+                    EditText editTextOCRApiGwUrl = (EditText) findViewById(R.id.text_input_ocr_api_gw_url);
+                    String clientOcrApiUrl = editTextOCRApiGwUrl.getText().toString();
+
+                    EditText editTextOcrSecretKey = (EditText) findViewById(R.id.text_input_ocr_secret_key);
+                    String clientOcrSecreteKey = editTextOcrSecretKey.getText().toString();
+
                     SharedPreferences sharedPref = getSharedPreferences("PREF", MODE_PRIVATE);
                     SharedPreferences.Editor editor = sharedPref.edit();
 
@@ -72,6 +86,8 @@ public class AuthActivity extends BaseActivity {
                     editor.putString("application_client_secret", clientSecretInputText);
                     editor.putString("chatbot_api_gw_url", clientApiUrl);
                     editor.putString("chatbot_secret_key", clientSecretKey);
+                    editor.putString("ocr_api_gw_url", clientOcrApiUrl);
+                    editor.putString("ocr_secret_key", clientOcrSecreteKey);
 
                     editor.commit();
 
@@ -79,10 +95,10 @@ public class AuthActivity extends BaseActivity {
                     textView1.setText("저장되었습니다.");
 
                 } catch (Exception e){
+                    TextView textView1 = (TextView) findViewById(R.id.textView_rlt) ;
+                    textView1.setText("저장 실패");
 
                 }
-
-
             }
         });
     }
